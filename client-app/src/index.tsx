9273a959a739e0c6fab1fd08852056ce3756e988
Francisco Albarran <francisco.albarran@audatex.com>
2022-01-04 19:53:03 -0600

    fix import semantic-ui react

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
+// Option 1: Package Manager It doesn't work!
+// import 'semantic-ui-css/semantic.min.css'; 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
