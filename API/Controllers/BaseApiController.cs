735723d920be12b20ffe6f7a7416890a9ae9d521
Francisco Albarran <francisco.albarran@audatex.com>
2022-01-06 13:51:23 -0600

    Section 4: Create CRUD app using CQRS && MediatR

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,6 +7,16 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices .GetService<IMediator>();
+
+        // --Other way to do the above! --
+        // protected IMediator Mediator { get; }
+        // public BaseApiController(IMediator mediator)
+        // {
+        //     Mediator = mediator;
+        // }
+        // public BaseApiController() {
+        // }
     }
 }
